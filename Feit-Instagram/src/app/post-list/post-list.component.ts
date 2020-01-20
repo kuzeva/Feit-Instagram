7e4d6b22a4477454e7bf1c49e61cad6f69ee3d0e
Bojana Kuzeva <kuzeva01@gmail.com>
2020-01-20 19:25:24 +0100

    user-details component finished . Added some icons to look visually better

--- Feit-Instagram/src/app/post-list/post-list.component.ts
+++ Feit-Instagram/src/app/post-list/post-list.component.ts
@@ -9,29 +9,19 @@ import { CommentsComponent } from '../comments/comments.component';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-pageNumber : number =1;
+
 posts : Post[] = [];
 
   constructor(public dialog : MatDialog, private apiService : FeitInstagramApiService) { 
     this.getPosts();
   }
 
-  // onClick(post : Post){
-  //   const dialogRef = this.dialog.open(CommentsComponent,{
-  //     width : '750px',
-  //     height : '700px',
-  //     data: {clickedPost : post}
-  //   });
-
-  //   dialogRef.afterClosed().subscribe(result =>{
-  //     console.log('The dialog was closed');
-  //   })
-  // }
   getPosts(){
     this.apiService.getPosts().subscribe((recievedPosts)=>{
     this.posts = recievedPosts;
   });
   }
+
 
   ngOnInit() {
   }
